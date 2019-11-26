781819c8921c4029a72d1b9314cbf04b7185fbcb
Long Nguyen <longnguyenctt@gmail.com>
2019-11-27 01:53:22 +0700

    implement button style type

--- src/components/Buttons/Button.stories.tsx
+++ src/components/Buttons/Button.stories.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Button from './Button';
 
-storiesOf('Button', module).add('default', () => (
-  <Button>Button</Button>
+storiesOf('Button', module)
+.add('primary', () => (
+  <div>
+    <Button styleType="primary" size="medium">Primary</Button>
+  </div>
+))
+.add('secondary', () => (
+  <Button styleType="secondary" size="medium">Secondary</Button>
+))
+.add('sucsess', () => (
+  <Button styleType="success" size="medium">Success</Button>
 ));
--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -1,41 +1,119 @@
-import React, { CSSProperties } from 'react';
+import React, { ReactNode } from 'react';
 import styled from '@emotion/styled';
 
 interface ButtonProps {
+  children: ReactNode,
+  styleType: ButtonStyleType;
+  size?: ButtonSizeType;
 }
 
-const Button: React.FC<ButtonProps> = ({ children }) => {
-  return <ButtonWrapper>{children}</ButtonWrapper>
+const Button: React.FC<ButtonProps> = ({ children, styleType, size }) => {
+  const styles = getStyle(styleType, size);
+  console.log(`%c HOVER_TEXT`, `background: ${styles.hover.backgroundColor}`);
+  
+  return <ButtonStyled styles={styles}>{children}</ButtonStyled>
 }
 
 export default Button;
 
-const ButtonWrapper = styled.button`
+//================Styled Component===================
+
+type ButtonStyleType = 'primary' | 'secondary' | 'success';
+type ButtonSizeType = 'small' | 'medium' | 'large';
+
+interface IButtonStyle {
+  static: {
+    backgroundColor: string;
+    color: string;
+    borderColor?: string;
+  };
+  hover: {
+    backgroundColor: string;
+    color: string;
+    borderColor?: string;
+  };
+  size?: number;
+}
+
+interface IButtonStyleProp {
+  styles: IButtonStyle;
+}
+
+const BUTTON_STYLE = {
+  primary: {
+    static: {
+      backgroundColor: 'rgba(88, 103, 221, 1)',
+      borderColor: 'rgba(88, 103, 221, 1)',
+      color: 'rgba(255, 255, 255, 1)'
+    },
+    hover: {
+      backgroundColor: 'rgba(56, 74, 215, 1)',
+      borderColor: 'rgba(56, 74, 215, 1)',
+      color: 'rgba(255, 255, 255, 1)'
+    },
+  },
+  secondary: {
+    static: {
+      backgroundColor: 'transparent',
+      borderColor: 'rgba(226, 229, 236, 1)',
+      color: 'rgba(89, 93, 110, 1)'
+    },
+    hover: {
+      backgroundColor: 'rgba(93, 120, 255, 1)',
+      borderColor: 'rgba(93, 120, 255, 1)',
+      color: 'rgba(255, 255, 255, 1)'
+    },
+  },
+  success: {
+    static: {
+      backgroundColor: 'rgba(10, 187, 135, 1)',
+      borderColor: 'rgba(10, 187, 135, 1)',
+      color: 'rgba(255, 255, 255, 1)'
+    },
+    hover: {
+      backgroundColor: 'rgba(8,151,109,1)',
+      borderColor: 'rgba(8,151,109,1)',
+      color: 'rgba(255, 255, 255, 1)'
+    },
+  }
+}
+
+const BUTTON_SIZE = {
+  small : {
+    size: 10,
+  },
+  medium : {
+    size: 10,
+  },
+  large : {
+    size: 10,
+  },
+}
+
+const getStyle = (styledType: ButtonStyleType, size?: ButtonSizeType) => {
+  return {...BUTTON_STYLE[styledType], ...BUTTON_SIZE[size || 'medium']};
+}
+
+const ButtonStyled = styled.button<IButtonStyleProp>`
   display: inline-block;
   font-weight: normal;
   text-align: center;
   vertical-align: middle;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
   user-select: none;
-  background-color: #5867dd;
-  border-color: #5867dd;
-  color: #fff;
-  border: 1px solid transparent;
+  background-color: ${props => props.styles.static.backgroundColor};
+  color: ${props => props.styles.static.color};
+  border: 1px solid ${props => props.styles.static.borderColor};
   padding: 0.65rem 1rem;
   font-size: 1rem;
   line-height: 1.5;
   border-radius: 0.25rem;
-  -webkit-transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, -webkit-box-shadow 0.15s ease-in-out;
-  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, -webkit-box-shadow 0.15s ease-in-out;
   transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out, -webkit-box-shadow 0.15s ease-in-out;
   cursor: pointer;
+  outline: none !important;
 
   &:hover {
-    color: #fff;
-    background-color: #2e40d4;
-    border-color: #293ccc;
+    color: ${props => props.styles.hover.color};
+    background-color: ${props => props.styles.hover.backgroundColor};
+    border: 1px solid ${props => props.styles.hover.borderColor};
   }
 `
